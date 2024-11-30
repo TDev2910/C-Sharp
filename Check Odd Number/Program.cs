331c4b7cb8c1766e53b8bcbd94799f0d7106a132
Su IT <phamchitrong2910@gmail.com>
2024-11-30 10:06:15 +0700

    Check ODD Number

---
+++ Check Odd Number/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_Odd_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for(int i = 0; i <= t;i++)
+            {
+                var data = Console.ReadLine().Split(' ');
+                int n = int.Parse(data[0]);
+                int k = int.Parse(data[1]);
+                Console.WriteLine($"Test {i}");
+                if(n<=0 || k > n)
+                {
+                    Console.WriteLine("No Result");
+                }
+                else
+                {
+                    for(int j = k ; j <= n; j++)
+                    {
+                        if(j % 2 !=0)
+                        {
+                            Console.WriteLine(j + " ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
