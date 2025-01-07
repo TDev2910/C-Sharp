a13f6931d5a4918f0ca8faf8fdcfecd510a7d399
Phạm Chí Trọng <122176490+TDev2910@users.noreply.github.com>
2025-01-08 01:16:12 +0700

    Add files via upload

---
+++ two-dimensional-array/Dev/Dev/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Dev
+{
+    internal class Program
+    {
+        static int[,] GetMatrix()
+        {
+            var data = Console.ReadLine().Split(' ');
+            var m = int.Parse(data[0]);
+            var n = int.Parse(data[1]);
+            if (m > 0 && n > 0)
+            {
+                int[,] matrix = new int[m, n];
+                var elements = Console.ReadLine().Split(' ');
+                //int index = 0;
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        matrix[i, j] = int.Parse(elements[i * n + j]);
+                    }
+                }
+                return matrix;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        static void ShowMatrix(int[,] matrix)
+        {
+            int m = matrix.GetLength(0);
+            int n = matrix.GetLength(1);
+            Console.WriteLine($"{m} {n}");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            var matrix = GetMatrix();
+            if (matrix != null)
+            {
+                ShowMatrix(matrix);
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
+        }
+    }
+}
